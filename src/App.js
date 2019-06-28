4b5a2f7798d6a7d746b7a3ac6e563c813c4df27d
Adarsh Pai <adarsh9pai@gmail.com>
2019-06-29 00:03:33 +0530

    two sections to go

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { Layout, Popover, Tabs, Comment, Card, Icon, Avatar, Alert, Typography, 
 import { Document, Page, pdfjs, View } from 'react-pdf';
 import profile from './assets/image.jpeg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Links from './links'
-import Contact from './contact'
+import Links from './links';
+import Contact from './contact';
+import Work from './work';
 let Markdown = require('react-markdown');
 
 const { Header, Content, Footer } = Layout;
@@ -20,7 +21,7 @@ const TabPane = Tabs.TabPane;
 
 class App extends Component {
     state = {
-        activeKey: "3"
+        activeKey: "2"
     };
     changeTabKey = key => {
         this.setState({ activeKey: key });
@@ -31,7 +32,7 @@ class App extends Component {
       <Layout className='layout'>
         <Header style = {{backgroundColor : '#1a1a1d'}}>
           <h3 style={{ color: 'white' }}>
-            <Icon type = "environment" style = {{color:'#ffff00'}} /> Arlington, TX 
+            <Icon type = "environment" style = {{color:'gray'}} /> DFW // BOM 
           </h3>
         </Header>
         <Content style = {{textAlign : 'center', backgroundColor : '#1a1a1d'}}>
@@ -41,7 +42,7 @@ class App extends Component {
                 <Avatar size={120} src='https://media.licdn.com/dms/image/C4E03AQEkAJNMel4j6Q/profile-displayphoto-shrink_200_200/0?e=1567036800&v=beta&t=a5uF7VhfIha7Ep7tZVrldUsMECNG60S_BciKsteIt9k'/>
                 <br />
                 <br />
-                <Title>Hi, I'm <strong style = {{color : '#ffff00'}}>Adarsh</strong></Title>
+                <Title>Hi, I'm Adarsh</Title>
                 <Text strong style = {{color: 'white'}}>
                 I am a budding <a href = "https://en.wikipedia.org/wiki/Software_engineer">Software Engineer</a> and rising junior majoring in <a href = "https://en.wikipedia.org/wiki/Computer_science">Computer Science</a> at the <a href = "https://www.uta.edu">University of Texas at Arlington</a>.<br/>
                 Before this, I spent 17 years of my life in different parts of <a href = "https://en.wikipedia.org/wiki/India">India</a>, predominantly in <a href = "https://en.wikipedia.org/wiki/Chennai">Chennai</a> and <a href = "https://en.wikipedia.org/wiki/Mumbai" >Mumbai</a>.<br/>
@@ -53,7 +54,7 @@ class App extends Component {
                 </Text>
               </TabPane>
               <TabPane tab="Work" key="2">
-                  Currently Updating. If you need some information immediately, contact me.
+                  <Work />
               </TabPane>
               <TabPane tab="Contact" key="3" style={{ padding: '20px 100px' }}>
                   <Contact />
---
+++ src/work.js
@@ -0,0 +1,13 @@
+import {Typography, Card} from 'antd';
+import React, { Component } from 'react';
+const {Title, Text} = Typography;
+
+const Work = () =>{
+    return(
+        <div>
+           <Text style = {{color:'white'}}>Currently updating this section as of June 28 2019. Meanwhile, download my resume </Text> <a href = "https://drive.google.com/file/d/1ixCbq8TRBpl6z3iS6sMsQhOL8vJAkmZx/view?usp=sharing" style = {{color: '#0079ff'}}>here.</a>
+  </div>
+    )
+}
+
+export default Work;
