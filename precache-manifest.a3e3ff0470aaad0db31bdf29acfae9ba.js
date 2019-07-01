45618c26a57e90f21ad9ebb7dcb0c320b3c56035
Adarsh Pai <adarsh9pai@gmail.com>
2019-07-01 13:43:04 +0530

    Updates

---
+++ precache-manifest.a3e3ff0470aaad0db31bdf29acfae9ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "746425cd4a4ee55741d5",
+    "url": "/static/css/main.50f823d0.chunk.css"
+  },
+  {
+    "revision": "746425cd4a4ee55741d5",
+    "url": "/static/js/main.fe4ee8dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "770cc5bb39a9d52dcfcb",
+    "url": "/static/css/2.5df8cd28.chunk.css"
+  },
+  {
+    "revision": "770cc5bb39a9d52dcfcb",
+    "url": "/static/js/2.e6b3be2e.chunk.js"
+  },
+  {
+    "revision": "ca59324b8b110502103ba6bf45cadac2",
+    "url": "/static/media/uta.ca59324b.png"
+  },
+  {
+    "revision": "de2381db280e28cede05e34c2f8e2023",
+    "url": "/static/media/utadss.de2381db.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b13d1e1e65a041a3cf0aa3e5d938d3a7",
+    "url": "/static/media/image.b13d1e1e.jpeg"
+  },
+  {
+    "revision": "61816438e121d6aa76566d63a402be89",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba88b9a1fdcd7ff07f3664e47562aa59.js"
+  "/precache-manifest.a3e3ff0470aaad0db31bdf29acfae9ba.js"
 );
 
 workbox.clientsClaim();
