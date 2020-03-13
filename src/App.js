682bddfdc7aabbe5d421ef114b4603431e34256e
Adarsh Pai <adarsh9pai@gmail.com>
2020-03-13 12:20:24 -0500

    .

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
         <Content>
           <QueueAnim delay={500} type='left' ease = 'easeInOutQuart'>
             <div key = {1}>
-              <Text className = "IntroductionText">I'm <mark><a href = "#">Adarsh Pai</a></mark>.<br/><br/></Text>
+              <Text className = "IntroductionText">Hi, I'm <mark><a href = "#">Adarsh Pai</a></mark>.<br/><br/></Text>
               <Text className = "IntroductionText">I will be spending this summer working on fascinating problems at <mark><a href = "https://www.citrix.com/">Citrix</a></mark>. Previously worked at <mark><a href = "https://www.emerson.com/">Emerson</a></mark>.<br/><br/></Text>
-              <Text className = "IntroductionText">Majoring in Computer Science with Honors at the <mark><a href = "https://www.uta.edu/">University of Texas at Arlington</a></mark>, Class of 2021.<br/><br/></Text>
+              <Text className = "IntroductionText">Computer Science at the <mark><a href = "https://www.uta.edu/">University of Texas at Arlington</a></mark>, Class of 2021.<br/><br/></Text>
               <Text className = "IntroductionText">Won a Nintendo Switch at a <mark><a href = "https://www.theshorthorn.com/news/for-uta-student-hackathon-team-winning-is-in-the-code/article_133c3eb8-6de8-11e9-a1f8-1bf4fa4b99df.html">hackathon</a></mark> once.<br/><br/></Text>
               <Text className = "IntroductionText">Raised in Mumbai, India.<br/><br/></Text>
               <Text className = "IntroductionText">Love Math, Problem Solving and Algorithms? Check out <mark><a href = "https://www.blazeoj.com/">BlazeJudge</a></mark>.<br/><br/></Text>
